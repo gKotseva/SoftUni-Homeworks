00865c83b69f8840def93d3f6cbc0866114c2a73
Gabriela Kotseva <gkotseva98@gmail.com>
2024-01-29 11:11:41 +0200

    Tasks - 12.01.2024

--- MySQL/Data Definition And Data Types/exercise/tasks.sql
+++ MySQL/Data Definition And Data Types/exercise/tasks.sql
@@ -11,3 +11,12 @@ create table towns (
 town_id int auto_increment primary key,
 name varchar(80)
 );
+
+-- 2.	Alter Minions Table
+-- Change the structure of the Minions table to have new column town_id that would be of the same type as the id column of towns table. Add new constraint that makes town_id foreign key and references to id column of towns table. Submit your create table query in Judge as MySQL run skeleton, run queries & check DB
+
+alter table minions
+add column town_id int;
+
+alter table minions
+add foreign key (town_id) references towns(id);
