b4c7b536997b3c969b5e1bf8e45cc60144936685
Gabriela Kotseva <gkotseva98@gmail.com>
2024-01-29 11:09:18 +0200

    Tasks - 16.01.2024

--- MySQL/Data Definition And Data Types/lab/tasks.sql
+++ MySQL/Data Definition And Data Types/lab/tasks.sql
@@ -1,5 +1,4 @@
 -- 1. Create tables
-
 -- Table "employees":
 -- •	id – INT, primary key, AUTO_INCREMENT;
 -- •	first_name – VARCHAR, NOT NULL; 
@@ -31,8 +30,19 @@ create table products (
 );
 
 -- 2. Insert Data in tables
-
 -- Inserting data can be done with a query too. To do that we use the "INSERT" clause. Populate the "employees" table with 3 test values.
 
 insert into employees(first_name, last_name) values('Gabi', 'Kotseva'), ('Kris', 'Hristov'), ('Maya', 'Kotseva');
+
+-- 3. Alter tables
+-- Altering the tables is done via the "ALTER TABLE" clause. Add a new column – "middle_name" to the "employees" table.
+
+alter table employees
+add middle_name varchar(50) not null;
+
+-- 4. Adding Constraints
+-- Create the connection via foreign key between the "products" and "categories" tables that you've created earlier. Make "category_id" foreign key linked to "id" in the "categories" table. 
+
+alter table products
+add constraint FOREIGN KEY (category_id) REFERENCES categories(id)
 
