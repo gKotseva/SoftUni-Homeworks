e026b141500d2219b9e17bbd04382d25e7f0d844
Gabriela Kotseva <gkotseva98@gmail.com>
2024-02-05 12:11:31 +0200

    Tasks - 16.01.2024

--- MySQL/Basic CRUD/exercise/tasks.sql
+++ MySQL/Basic CRUD/exercise/tasks.sql
@@ -31,3 +31,37 @@ select concat(first_name, + '.', last_name, '@softuni.bg') as full_email_address
 -- Write a SQL query to find all different employee's salaries. Show only the salaries.
 
 select distinct salary from employees;
+
+-- 7. Find all Information About Employees
+-- Write a SQL query to find all information about the employees whose job title is "Sales Representative". Sort the information by id.
+
+select * from employees
+where job_title = 'Sales Representative'
+order by employee_id;
+
+-- 8. Find Names of All Employees by salary in Range
+-- Write a SQL query to find the first name, last name and job title of all employees whose salary is in the range [20000, 30000]. Sort the information by id.
+
+select first_name, last_name, job_title from employees
+where salary >= 20000 and salary <= 30000
+order by employee_id;
+
+-- 9. Find Names of All Employees
+-- Write a SQL query to find the full name of all employees whose salary is 25000, 14000, 12500 or 23600. Full Name is combination of first, middle and last name (separated with single space) and they should be in one column called "Full Name".
+
+select concat_ws(' ', first_name, middle_name, last_name) as 'Full Name' from employees
+where salary = 25000 or salary=14000 or salary=12500 or salary=23600;
+
+-- 10. Find All Employees Without Manager
+-- Write a SQL query to find first and last names about those employees that does not have a manager.
+
+select first_name, last_name from employees
+where manager_id is null;
+
+-- 11. Find All Employees with salary More Than 50000
+-- Write a SQL query to find first name, last name and salary of those employees who has salary more than 50000. Order them in decreasing order by salary.
+
+select first_name, last_name, salary from employees
+where salary > 50000
+order by salary desc;
+
