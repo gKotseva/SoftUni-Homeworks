96fe4954967bcb85ea8f5671b7f0a0269114eee4
Gabriela Kotseva <gkotseva98@gmail.com>
2024-02-05 12:09:36 +0200

    Tasks - 16.01.2024

--- MySQL/Basic CRUD/exercise/tasks.sql
+++ MySQL/Basic CRUD/exercise/tasks.sql
@@ -15,3 +15,19 @@ order by department_id;
 
 select first_name, last_name, salary from employees
 order by employee_id;
+
+-- 4. Find Full Name of Each Employee
+-- Write SQL query to find the first, middle and last name of each employee. Sort the information by id.
+
+select first_name, middle_name, last_name from employees
+order by employee_id;
+
+-- 5. Find Email Address of Each Employee
+-- Write a SQL query to find the email address of each employee. (by his first and last name). Consider that the email domain is softuni.bg. Emails should look like "John.Doe@softuni.bg". The produced column should be named "full_ email_address".
+
+select concat(first_name, + '.', last_name, '@softuni.bg') as full_email_address from employees
+
+-- 6. Find All Different Employee's Salaries
+-- Write a SQL query to find all different employee's salaries. Show only the salaries.
+
+select distinct salary from employees;
