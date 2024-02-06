282d3c4940199b014f420bf481a8753300749e0e
Gabriela Kotseva <gkotseva98@gmail.com>
2024-02-06 14:22:52 +0200

    Tasks - 23.01.2024

--- MySQL/Built-in Functions/exercise/tasks.sql
+++ MySQL/Built-in Functions/exercise/tasks.sql
@@ -45,3 +45,41 @@ order by name asc;
 select * from towns
 where name not like 'r%' and name not like 'b%' and name not like 'd%'
 order by name asc;
+
+-- 8. Create View Employees Hired After 2000 Year
+-- Write a SQL query to create view v_employees_hired_after_2000 with the first and the last name of all employees hired after 2000 year. Select all from the created view.
+
+CREATE VIEW `v_employees_hired_after_2000` AS
+    SELECT 
+        `first_name`, `last_name`
+    FROM
+        `employees`
+    WHERE
+        YEAR(`hire_date`) > 2000;
+
+-- 9. Length of Last Name
+-- Write a SQL query to find the first and last names of all employees whose last name is exactly 5 characters long.
+
+select first_name, last_name from employees
+where length(last_name) = 5;
+
+-- 10. Countries Holding 'A' 3 or More Times
+-- Find all countries that hold the letter 'A' in their name at least 3 times (case insensitively), sorted by ISO code. Display the country name and the ISO code.
+
+SELECT 
+    `country_name`, `iso_code`
+FROM
+    `countries`
+WHERE
+    (CHAR_LENGTH(`country_name`) - CHAR_LENGTH(REPLACE(LOWER(`country_name`), 'a', ''))) >= 3
+ORDER BY `iso_code`;
+
+-- 11. Mix of Peak and River Names
+-- Combine all peak names with all river names, so that the last letter of each peak name is the same as the first letter of its corresponding river name. Display the peak name, the river name, and the obtained mix(converted to lower case). Sort the results by the obtained mix alphabetically.
+
+select peak_name, river_name, LOWER(CONCAT(`peak_name`, SUBSTRING(`river_name`, 2))) AS 'mix'
+from peaks, rivers
+where lower(right(peak_name, 1)) = lower(left(river_name, 1))
+order by mix asc;
+
+
