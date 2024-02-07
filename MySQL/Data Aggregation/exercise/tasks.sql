8e93f392e55aa7edc6086dd8a6dee2d899192f23
Gabriela Kotseva <gkotseva98@gmail.com>
2024-02-07 17:32:13 +0200

    Tasks - 25.01.2024

--- MySQL/Data Aggregation/exercise/tasks.sql
+++ MySQL/Data Aggregation/exercise/tasks.sql
@@ -36,3 +36,60 @@ where `magic_wand_creator` = 'Ollivander family'
 group by deposit_group
 order by deposit_group;
 
+-- 7. Deposits Filter
+-- Select all deposit groups and its total deposit sum but only for the wizards who has their magic wand crafted by Ollivander family. After this, filter total deposit sums lower than 150000. Order by total deposit sum in descending order.
+
+select deposit_group, sum(deposit_amount) as 'total_sum' from wizzard_deposits
+where `magic_wand_creator` = 'Ollivander family'
+group by deposit_group
+having `total_sum` < 150000
+order by `total_sum` desc;
+
+-- 8. Deposit Charge
+-- Create a query that selects:
+-- • Deposit group
+-- • Magic wand creator
+-- • Minimum deposit charge for each group
+-- Group by deposit_group and magic_wand_creator.
+-- Select the data in ascending order by magic_wand_creator and deposit_group.
+
+select deposit_group, magic_wand_creator, min(deposit_charge) as 'min_deposit_charge' from wizzard_deposits
+group by deposit_group, magic_wand_creator
+order by magic_wand_creator, deposit_group;
+
+-- 9. Age Groups
+-- Write down a query that creates 7 different groups based on their age.
+-- Age groups should be as follows:
+-- • [0-10]
+-- • [11-20]
+-- • [21-30]
+-- • [31-40]
+-- • [41-50]
+-- • [51-60]
+-- • [61+]
+-- The query should return:
+-- • Age groups
+-- • Count of wizards in it
+-- Sort result by increasing size of age groups.
+
+select
+case 
+	when age >= 0 and age <= 10 then '[0-10]'
+    when age >= 11 and age <= 20 then '[11-20]'
+    when age >= 21 and age <= 30 then '[21-30]'
+    when age >= 31 and age <= 40 then '[31-40]'
+    when age >= 41 and age <= 50 then '[41-50]'
+    when age >= 51 and age <= 60 then '[51-60]'
+    else '[61+]'
+end as age_group, count(age) as wizard_count
+from wizzard_deposits
+group by `age_group`
+order by `age_group`;
+
+-- 10. First Letter
+-- Write a query that returns all unique wizard first letters of their first names only if they have deposit of type Troll Chest. Order them alphabetically. Use GROUP BY for uniqueness.
+
+select left(first_name, 1) as first_letter from wizzard_deposits
+where deposit_group = 'Troll Chest'
+group by first_letter
+order by first_letter;
