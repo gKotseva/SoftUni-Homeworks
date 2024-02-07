cdee74cd3929dcd3d95308c00dded094c900ad2c
Gabriela Kotseva <gkotseva98@gmail.com>
2024-02-07 17:35:16 +0200

    Tasks - 25.01.2024

--- MySQL/Data Aggregation/exercise/tasks.sql
+++ MySQL/Data Aggregation/exercise/tasks.sql
@@ -93,3 +93,65 @@ select left(first_name, 1) as first_letter from wizzard_deposits
 where deposit_group = 'Troll Chest'
 group by first_letter
 order by first_letter;
+
+-- 11. Average Interest
+-- Mr. Bodrog is highly interested in profitability. He wants to know the average interest of all deposits groups split by whether the deposit has expired or not. But that's not all. He wants you to select deposits with start date after 01/01/1985. Order the data descending by Deposit Group and ascending by Expiration Flag.
+
+select deposit_group, is_deposit_expired, avg(deposit_interest) as average_interest from wizzard_deposits
+where deposit_start_date > '1985-01-01'
+group by deposit_group, is_deposit_expired
+order by deposit_group desc, is_deposit_expired;
+
+-- 12. Employees Minimum Salaries
+-- That's it! You no longer work for Mr. Bodrog. You have decided to find a proper job as an analyst in SoftUni.
+-- It's not a surprise that you will use the soft_uni database.
+-- Select the minimum salary from the employees for departments with ID (2,5,7) but only for those who are hired after 01/01/2000. Sort result by department_id in ascending order.
+-- Your query should return:
+-- • department_id
+
+select department_id, min(salary) from employees
+where hire_date > date('2000-01-01') and (department_id = 5 or department_id = 2 or department_id = 7)
+group by department_id;
+
+-- 13. Employees Average Salaries
+-- Select all high paid employees who earn more than 30000 into a new table. Then delete all high paid employees who have manager_id = 42 from the new table. Then increase the salaries of all high paid employees with department_id = 1 with 5000 in the new table. Finally, select the average salaries in each department from the new table. Sort result by department_id in increasing order.
+
+SELECT 
+    `department_id`,
+    CASE
+        WHEN `department_id` = 1 THEN AVG(`salary`) + 5000
+        ELSE AVG(`salary`)
+    END AS 'avg_salary'
+FROM
+    `employees`
+WHERE
+    `salary` > 30000 AND `manager_id` != 42
+GROUP BY `department_id`
+ORDER BY `department_id`;
+
+-- 14. Employees Maximum Salaries
+-- Find the max salary for each department. Filter those which have max salaries not in the range 30000 and 70000. Sort result by department_id in increasing order.
+
+select department_id, max(salary) as max_salary from employees
+group by department_id
+having not max_salary between 30000 and 70000
+order by department_id;
+
+-- 15. Employees Count Salaries
+-- Count the salaries of all employees who don't have a manager.
+
+select count(salary) as '' from employees
+where manager_id is null;
+
+-- 16. 3rd Highest Salary*
+-- Find the third highest salary in each department if there is such. Sort result by department_id in increasing order.
+
+select department_id, (
+	select distinct salary from employees
+	where e.department_id = department_id
+	order by salary desc limit 1 offset 2) as third_highest_salary
+from employees e
+group by department_id
+having third_highest_salary is not null
+order by department_id;
+
