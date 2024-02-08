b69c9d2e2a94b8aeb4f4d77d013d2ffaf646bf03
Gabriela Kotseva <gkotseva98@gmail.com>
2024-02-08 13:37:41 +0200

    Tasks - 28.01.2024

--- MySQL/Table Relations/exercise/tasks.sql
+++ MySQL/Table Relations/exercise/tasks.sql
@@ -93,3 +93,143 @@ create table students_exams(
 insert into students(name) values('Mila'), ('Toni'), ('Ron');
 insert into exams(exam_id, name) values(101, 'Spring MVC'), (102, 'Neo4j'), (103, 'Oracle 11g');
 insert into students_exams(student_id, exam_id) values (1, 101), (1, 102), (2, 101), (3, 103), (2, 102), (2, 103);
+
+-- 4. Self-Referencing
+-- Create a single table as follows. Use appropriate data types.
+-- Insert the data from the example above.
+-- · Add primary and foreign keys.
+-- · The foreign key should be between manager_id and teacher_id.
+
+create table teachers(
+	teacher_id int primary key not null,
+    name varchar(50) not null,
+    manager_id int,
+    
+    constraint fk_teacher_id_manager_id
+		foreign key(manager_id)
+        references teachers(teacher_id)
+);
+
+insert into teachers(teacher_id, name, manager_id) 
+values (101, 'John', null), (105, 'Mark', 101), (106, 'Greta', 101), (102, 'Maya', 106), (103, 'Silvia', 106), (104, 'Ted', 105)
+
+-- 5. Online Store Database
+-- Create a new database and design the following structure:
+
+create table item_types(
+	item_type_id int(11) auto_increment primary key,
+    name varchar(50)
+);
+
+create table cities(
+	city_id int(11) auto_increment primary key,
+    name varchar(50)
+);
+
+create table items(
+	item_id int(11) auto_increment primary key,
+    name varchar(50),
+    item_type_id int,
+    
+    constraint fk_item_id_to_item_types
+		foreign key (item_type_id)
+        references item_types(item_type_id)
+);
+
+create table customers(
+	customer_id int(11) auto_increment primary key,
+    name varchar(50),
+    birthday date,
+    city_id int,
+    
+	constraint fk_city_id_to_cities
+		foreign key (city_id)
+        references cities(city_id)
+    
+);
+
+create table orders(
+	order_id int(11) auto_increment primary key not null,
+    customer_id int(11),
+    
+	constraint fk_customer_id_to_customers
+		foreign key (customer_id)
+        references customers(customer_id)
+);
+
+create table order_items(
+	order_id int(11),
+    item_id int(11),
+    
+    primary key(order_id, item_id),
+    
+	constraint fk_item_id_to_items
+		foreign key (item_id)
+        references items(item_id),
+        
+	constraint fk_order_id_to_orders
+		foreign key (order_id)
+        references orders(order_id)
+);
+
+-- 6. University Database
+-- Create a new database and design the following structure:
+
+create table subjects(
+	subject_id int auto_increment primary key,
+    subject_name varchar(50)
+);
+
+create table majors(
+	major_id int auto_increment primary key,
+    name varchar(50)
+);
+
+create table students(
+	student_id int auto_increment primary key,
+    student_number varchar(12),
+    student_name varchar(50),
+    major_id int,
+    
+	constraint fk_major_id
+		foreign key(major_id)
+        references majors(major_id)
+);
+
+create table agenda(
+	student_id int,
+    subject_id int,
+    
+    primary key(student_id, subject_id),
+    
+    constraint fk_student_id_agenda
+		foreign key(student_id)
+        references students(student_id),
+	
+    constraint fk_subject_id
+		foreign key(subject_id)
+        references subjects(subject_id)
+);
+
+create table payments(
+	payment_id int auto_increment primary key,
+    payment_date date, 
+    payment_amount decimal(8, 2),
+    student_id int,
+        
+	constraint fk_student_id
+		foreign key(student_id)
+        references students(student_id)
+);
+
+-- 7. Peaks in Rila
+-- Display all peaks for "Rila" mountain_range. Include:
+-- · mountain_range
+-- · peak_name
+-- · peak_elevation
+-- Peaks should be sorted by peak_elevation descending.
+
+select mountains.mountain_range, peak_name, elevation as peak_elevation from peaks
+join mountains on peaks.mountain_id=mountains.id
+where mountains.mountain_range = 'Rila'
+order by peak_elevation desc;
