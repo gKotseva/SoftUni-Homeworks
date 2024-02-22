34b411fb7b234e9452ec9178293d655ad3f9304d
Gabriela Kotseva <gkotseva98@gmail.com>
2024-02-22 12:01:39 +0200

    Tasks - 02.02.2024

--- MySQL/Database Programmability and Transactions/exercises/tasks.sql
+++ MySQL/Database Programmability and Transactions/exercises/tasks.sql
@@ -118,3 +118,125 @@ reads sql data
 begin
 	return initial * POW(1 + yearly_interest_rate, number_of_years);
 end 
+
+-- 11. Calculating Interest
+-- Your task is to create a stored procedure usp_calculate_future_value_for_account that accepts as parameters – id of account and interest rate. The procedure uses the function from the previous problem to give an interest to a person's account for 5 years, along with information about his/her account id, first name, last name and current balance as it is shown in the example below. It should take the account_id and the interest_rate as parameters. Interest rate should have precision up to 0.0001, same as the calculated balance after 5 years. Be extremely careful to achieve the desired precision!
+
+create function ufn_calculate_future_value(initial decimal(10,4), yearly_interest_rate decimal(10,4), number_of_years int)
+returns decimal(10,4)
+reads sql data
+begin
+	return initial * POW(1 + yearly_interest_rate, number_of_years);
+end ;
+
+create procedure usp_calculate_future_value_for_account(acc_id int, interest_rate decimal(10,4))
+begin
+select a.id as account_id, 
+ah.first_name, ah.last_name, 
+a.balance as current_balance, 
+ufn_calculate_future_value(a.balance, interest_rate, 5) as balance_in_5_years 
+from account_holders ah
+join accounts a on ah.id=a.account_holder_id
+where a.id=acc_id;
+end ;
+
+-- 12. Deposit Money
+-- Add stored procedure usp_deposit_money(account_id, money_amount) that operate in transactions. Follow us:
+-- Make sure to guarantee valid positive money_amount with precision up to fourth sign after decimal point. The procedure should produce exact results working with the specified precision.
+
+create procedure usp_deposit_money(acc_id int, money_amount decimal(10,4))
+begin
+	start transaction;
+    if ((select count(*) from accounts where id = acc_id) <> 1 or money_amount < 0) then
+    rollback;
+    else update accounts set balance = balance + money_amount where id = acc_id;
+    commit;
+    end if;
+end
+
+-- 13. Withdraw Money
+-- Add stored procedures usp_withdraw_money(account_id, money_amount) that operate in transactions.
+-- Make sure to guarantee withdraw is done only when balance is enough and money_amount is valid positive number. Work with precision up to fourth sign after decimal point. The procedure should produce exact results working with the specified precision.
+
+create procedure usp_withdraw_money(acc_id int, money_amount decimal(19,4))
+begin
+	start transaction;
+    if ((select balance from accounts where id = acc_id) < money_amount or money_amount < 0) then
+    rollback;
+    else 
+    update accounts set balance = balance - money_amount where id = acc_id;
+    commit;
+    end if;
+end 
+
+-- 14. Money Transfer
+-- Write stored procedure usp_transfer_money(from_account_id, to_account_id, amount) that transfers money from one account to another. Consider cases when one of the account_ids is not valid, the amount of money is negative number, outgoing balance is enough or transferring from/to one and the same account. Make sure that the whole procedure passes without errors and if error occurs make no change in the database.
+-- Make sure to guarantee exact results working with precision up to fourth sign after decimal point.
+
+create procedure usp_transfer_money(from_account_id int, to_account_id int, amount decimal(19,4))
+begin
+	start transaction;
+    if((select count(*) from accounts where id=from_account_id) <> 1) or ((select count(*) from accounts where id=to_account_id) <> 1) or ((select balance from accounts where id = from_account_id) < amount) or amount <= 0 then
+    rollback;
+    else 
+    update accounts set balance = balance - amount where id=from_account_id;
+	update accounts set balance = balance + amount where id=to_account_id;
+    commit;
+    end if;
+end 
+
+-- 15. Log Accounts Trigger
+-- Create another table – logs(log_id, account_id, old_sum, new_sum). Add a trigger to the accounts table that enters a new entry into the logs table every time the sum on an account changes.
+
+create table logs(
+	log_id int auto_increment primary key,
+    account_id int not null,
+    old_sum decimal(19,4),
+    new_sum decimal(19,4)
+);
+
+create trigger tr_changes_to_sum
+after update on accounts
+for each row
+begin
+	insert into logs(account_id, old_sum, new_sum) values (old.id, old.balance, new.balance);
+end 
+
+-- 16. Emails Trigger
+-- Create another table – notification_emails(id, recipient, subject, body). Add a trigger to logs table to create new email whenever new record is inserted in logs table. The following data is required to be filled for each email:
+-- · recipient – account_id
+-- · subject – "Balance change for account: {account_id}"
+-- · body - "On {date (current date)} your balance was changed from {old} to {new}."
+
+create table logs (
+    log_id int primary key auto_increment,
+    account_id int not null,
+    old_sum decimal(19,4),
+    new_sum decimal(19,4)
+);
+
+create trigger tr_update
+after update
+on accounts
+for each row
+begin
+    insert into logs(account_id, old_sum, new_sum)
+    values (old.id, old.balance, new.balance);
+end;
+
+create table notification_emails (
+    id int primary key auto_increment,
+    recipient int not null,
+    subject varchar(2000),
+    body text
+);
+
+create trigger tr_email_notification
+after insert on logs
+for each row
+begin
+    insert into notification_emails(recipient, subject, body)
+    values(new.account_id, 
+        concat('Balance change for account: ', new.account_id), 
+        concat('On ',  date_format(now(), '%b, %d, %Y at %r'), ' your balance was changed from ', round(new.old_sum,0), ' to ', round(new.new_sum, 0), '.'));
+end;
